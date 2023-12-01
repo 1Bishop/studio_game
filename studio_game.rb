acd728893e59202ce1cbfcb453a12213c6d687b4
1Bishop <chris@bishop.dev>
2023-12-01 15:52:06 -0500

    Finished Lesson 6

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,18 @@
 name = "Finn"
 health  = 60
 
-puts "#{name.capitalize} has a health of #{health}."
-puts "#{name.capitalize}'s health is #{health}.".center(50, '*')
-puts "#{name.capitalize}".ljust(30, '.') + "#{health} health"
+def say_hello(name, health=100)
+  "I'm #{name.capitalize} with a health of #{health} as of #{Time.now.strftime("%I:%M:%S")}"
+end
 
-puts health**2
-puts health.to_s.reverse
+def take_damage(name:, health:, damage:, bonus:, points:)
+  health -= damage
+  puts "#{name.capitalize} took #{damage} damage and has #{health} health remaining."
+end
+
+puts say_hello(name, health)
+puts take_damage(name: "larry", health: 60, damage: 30, bonus: 10, points: 5)
+
 
 puts "Let's play a game!"
 3.times do |i|
