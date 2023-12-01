2f270577ea560d90b71c208e54938406e376c850
1Bishop <chris@bishop.dev>
2023-12-01 16:51:30 -0500

    Finished Lesson 8

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,15 @@
 class Player
+
+  attr_accessor :name
+  attr_reader :health
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def boost
@@ -15,28 +19,18 @@ class Player
   def drain
     @health -= 10
   end
+
+  def score
+    @health + @name.length
+  end
 end
 
 
 # Test Code:
-player_1 = Player.new("finn", 60)
-puts player_1
-player_1.boost
-puts player_1
-
-player_2 = Player.new("lucy", 90)
-puts player_2
-player_2.drain
-puts player_2
-
-player_3 = Player.new("jase")
-puts player_3
-player_3.boost
-player_3.boost
-puts player_3
-
 player_4 = Player.new("alex", 125)
-puts player_4
-player_4.drain
-player_4.boost
+puts player_4.name
+player_4.name = "Alexander"
+puts player_4.name
+puts player_4.health
+puts player_4.score
 puts player_4
