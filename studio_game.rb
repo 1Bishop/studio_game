598d0244ebf213252b051bf393f70ad1e5d765cb
1Bishop <chris@bishop.dev>
2023-12-01 16:47:49 -0500

    Finished Lesson 7

--- studio_game.rb
+++ studio_game.rb
@@ -1,22 +1,42 @@
-name = "Finn"
-health  = 60
+class Player
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
 
-def say_hello(name, health=100)
-  "I'm #{name.capitalize} with a health of #{health} as of #{Time.now.strftime("%I:%M:%S")}"
+  def to_s
+    "I'm #{@name} with a health of #{@health}."
+  end
+
+  def boost
+    @health += 15
+  end
+
+  def drain
+    @health -= 10
+  end
 end
 
-def take_damage(name:, health:, damage:, bonus:, points:)
-  health -= damage
-  puts "#{name.capitalize} took #{damage} damage and has #{health} health remaining."
-end
 
-puts say_hello(name, health)
-puts take_damage(name: "larry", health: 60, damage: 30, bonus: 10, points: 5)
+# Test Code:
+player_1 = Player.new("finn", 60)
+puts player_1
+player_1.boost
+puts player_1
 
+player_2 = Player.new("lucy", 90)
+puts player_2
+player_2.drain
+puts player_2
 
-puts "Let's play a game!"
-3.times do |i|
-  puts 3 - i
-end
+player_3 = Player.new("jase")
+puts player_3
+player_3.boost
+player_3.boost
+puts player_3
 
-puts "GO!"
+player_4 = Player.new("alex", 125)
+puts player_4
+player_4.drain
+player_4.boost
+puts player_4
